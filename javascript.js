62bcd0dfcc0d7a2793537054f6a19a751145db45
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2024-03-25 09:02:06 +0200

    Add button functionality to card elems and clean up code

--- javascript.js
+++ javascript.js
@@ -4,33 +4,41 @@ class Book {
       this.author = author;
       this.pages = pages;
       this.read = read;
-    }
+    };
 
     info() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     };
+
+    toggleRead() {
+      const matchedBtn = document.querySelector(`button[data-read-btn-title="${this.title}"]`);
+      if(this.read) {
+        matchedBtn.classList.remove('read');
+        this.read = false;
+      } else {
+        this.read = true;
+        matchedBtn.classList.add('read');
+      }
+    };
 };
 
+// Template
 // const hp = new Book('Harry Potter and the Phoenix Order', 'J.K. Rowling', 986, 'not read yet');
-
 // console.log(hp.info());
 
 const myBooks = [];
-
 const openModalBtn = document.querySelector('#open-btn');
 const modal = document.querySelector('dialog.modal');
-
 const bookTitleInput = document.querySelector('input#title');
 const bookAuthorInput = document.querySelector('input#author');
 const bookPagesInput = document.querySelector('input#pages');
 const bookCompleteCheckbox = document.querySelector('input#complete');
 const bookCardsContainer = document.querySelector('#book-cards-container');
-
+const form = document.querySelector('form');
+const requiredInputs = document.querySelectorAll('[required]');
 const closeBtn = document.querySelector('#close-btn');
 const submitBtn = document.querySelector('#submit-btn');
-
 const bookExistsError = document.querySelector('div#book-exists-error')
-
 
 function addBookToLibrary(e) {
   if (bookTitleInput.validity.valid && bookAuthorInput.validity.valid && bookPagesInput.validity.valid) {
@@ -41,22 +49,20 @@ function addBookToLibrary(e) {
         bookExistsError.style.display = 'none';
       } else {
         e.preventDefault();
-        console.log(bookExistsError);
         bookExistsError.style.display = 'block';
       }
   }
 }
 
-
 function createCardsFromLibrary(books) {
   const cards = document.querySelectorAll('.card');
 
   books.forEach((book) => {
-    const isCardCreated = Array.from(cards).some((card) => card.dataset.book === book.title);
+    const isCardCreated = Array.from(cards).some((card) => card.dataset.cardTitle === book.title);
 
     if (!isCardCreated) {
       const card = document.createElement('div');
-      card.setAttribute('data-book', book.title)
+      card.setAttribute('data-card-title', book.title)
       card.classList.add('card');
       bookCardsContainer.appendChild(card);
 
@@ -86,26 +92,45 @@ function createCardsFromLibrary(books) {
       readBtn.classList.add('btn');
       if(book.read === true) readBtn.classList.add('read');
       readBtn.textContent = 'Read';
-      bookButtonSection.appendChild(readBtn)
+      bookButtonSection.appendChild(readBtn);
+      readBtn.setAttribute('data-read-btn-title', book.title);
+      readBtn.addEventListener('click', book.toggleRead.bind(book));
 
       const removeBtn = document.createElement('button')
       removeBtn.classList.add('btn');
       removeBtn.textContent = 'X';
       bookButtonSection.appendChild(removeBtn);  
+      removeBtn.setAttribute('id', 'remove-btn');
+      removeBtn.setAttribute('data-remove-btn-title', book.title);
+      removeBtn.addEventListener('click', removeCard.bind(removeBtn));
       }
   });
 }
 
-function closeModal() {
-  modal.close();
+function removeCard(e) {
+  const matchedBook = myBooks.findIndex((book) => book.title === this.dataset.removeBtnTitle);
+  const matchedCard = document.querySelector(`div[data-card-title=${this.dataset.removeBtnTitle}]`);
+
+  myBooks.splice(matchedBook, 1);
+  matchedCard.parentElement.removeChild(matchedCard);
 }
 
-window.addEventListener('click', (e) => {
-  if(e.target.id === 'remove-btn') {
-    
-    
-    console.log('yoooo');
-  }
+//Reset the validation after the form is submitted
+form.addEventListener('submit', (e) => {
+  // Temporarily remove the required attribute from inputs
+  requiredInputs.forEach(input => {
+      input.removeAttribute('required');
+  });
+
+  // Reset the form
+  form.reset();
+
+  // Restore the required attribute to inputs after a short delay
+  setTimeout(() => {
+    requiredInputs.forEach(input => {
+          input.setAttribute('required', '');
+      });
+  }, 100);
 });
 
 submitBtn.addEventListener('click', function (e) {
